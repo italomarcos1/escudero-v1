abdcc3c23257e471f2d274f013dd8ee31560886c
Italo <italomarcos0010@gmail.com>
2023-04-14 17:43:44 -0300

    done

--- src/pages/home/index.page.tsx
+++ src/pages/home/index.page.tsx
@@ -1,9 +1,30 @@
-import { Heading } from '@ignite-ui/react'
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+import Image from 'next/image'
+
+import previewImage from '../../assets/preview.png'
 
 export default function Home() {
   return (
-    <>
-      <Heading as="h1">Escudero</Heading>
-    </>
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento Descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário de exibição"
+        />
+      </Preview>
+    </Container>
   )
 }
