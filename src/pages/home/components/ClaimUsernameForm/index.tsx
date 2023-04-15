eb3cbeff0f852f283228294f737390212de27905
Italo <italomarcos0010@gmail.com>
2023-04-15 10:19:44 -0300

    done

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,35 @@
+import { Button, TextInput } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Form } from './styles'
+
+const claimUsernameForm = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameForm>
+
+export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log('d', data)
+  }
+
+  return (
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
+      <Button size="sm" type="submit">
+        Reservar
+        <ArrowRight />
+      </Button>
+    </Form>
+  )
+}
